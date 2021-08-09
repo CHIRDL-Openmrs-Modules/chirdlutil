b7cb6911bc694002e455c3a09816817288952056
Steve McKee <sjmckee@iu.edu>
2021-08-09 07:30:56 -0400

    CHICA-1550: Commit before merge.

--- api/src/main/java/org/openmrs/module/chirdlutil/tools/CreateFlowsheetConceptMapping.java
+++ api/src/main/java/org/openmrs/module/chirdlutil/tools/CreateFlowsheetConceptMapping.java
@@ -1,10 +1,14 @@
 package org.openmrs.module.chirdlutil.tools;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 public class CreateFlowsheetConceptMapping {
 	
@@ -17,6 +21,31 @@ public class CreateFlowsheetConceptMapping {
 	private static final String EXAMPLE_MESSAGE = "CreateFlowsheetConceptMapping -output C:\\test\\mappings.csv -input "
 			+ "C:\\test\\flowsheetMapping.csv.csv -location ABCD -displayConceptSource \"ABCD Flowsheet Display\" "
 			+ "-codeConceptSource \"ABCD Flowsheet Code\"";
+	
+	public void createFlowsheetConceptMappingFile(FlowsheetConceptMappingArgs mappingArgs) throws Exception {
+		try (InputStreamReader inStreamReader = new InputStreamReader(new FileInputStream(mappingArgs.getInput()));) {
+			CSVReader reader = new CSVReader(inStreamReader, ',');
+			// The first line contains the following text: Flowsheet System OID
+			reader.readNext();
+			
+			// The second line contains the flowsheet system OID
+			String[] items = reader.readNext();
+			if (items == null || items.length == 0) {
+				throw new Exception("The file does not contain a flowsheet system OID on line 2, column 1");
+			}
+			
+			// The next line is blank
+			reader.readNext();
+			
+			// The next line contains the headers
+			items = reader.readNext();
+			if (items == null || items.length == 0) {
+				throw new Exception("The file does not contains the headers expected on line 4");
+			}
+			
+			
+		}
+	}
 	
 	private static FlowsheetConceptMappingArgs createFlowsheetConceptMappingArgs(String[] args) {
 		FlowsheetConceptMappingArgs mappingArgs = new FlowsheetConceptMappingArgs();
@@ -66,7 +95,7 @@ public class CreateFlowsheetConceptMapping {
 				if (isInput) {
 					inputFile = new File(arg);
 					if (!inputFile.exists() || !inputFile.canRead() || inputFile.isDirectory()) {
-						LOG.error("Cannot locate or read input file " + arg);
+						System.err.println("Cannot locate or read input file " + arg);
 						return null;
 					}
 					
@@ -88,32 +117,32 @@ public class CreateFlowsheetConceptMapping {
 		}
 		
 		if (outputFile == null) {
-			LOG.error("No output file specified.");
-			LOG.error(EXAMPLE_MESSAGE);
+			System.err.println("No output file specified.");
+			System.err.println(EXAMPLE_MESSAGE);
 			return null;
 		}
 			
 		if (inputFile == null) {
-			LOG.error("No input files specified.");
-			LOG.error(EXAMPLE_MESSAGE);
+			System.err.println("No input files specified.");
+			System.err.println(EXAMPLE_MESSAGE);
 			return null;
 		}
 		
 		if (StringUtils.isBlank(location)) {
-			LOG.error("No location specified.");
-			LOG.error(EXAMPLE_MESSAGE);
+			System.err.println("No location specified.");
+			System.err.println(EXAMPLE_MESSAGE);
 			return null;
 		}
 		
 		if (StringUtils.isBlank(displayConceptSource)) {
-			LOG.error("No displayConceptSource specified.");
-			LOG.error(EXAMPLE_MESSAGE);
+			System.err.println("No displayConceptSource specified.");
+			System.err.println(EXAMPLE_MESSAGE);
 			return null;
 		}
 		
 		if (StringUtils.isBlank(codeConceptSource)) {
-			LOG.error("No codeConceptSource specified.");
-			LOG.error(EXAMPLE_MESSAGE);
+			System.err.println("No codeConceptSource specified.");
+			System.err.println(EXAMPLE_MESSAGE);
 			return null;
 		}
 		
@@ -127,8 +156,8 @@ public class CreateFlowsheetConceptMapping {
 	 */
 	public static void main(String[] args) {
 		if (args == null || args.length == 0) {
-            LOG.error("Please specify the output and input files.");
-            LOG.error(EXAMPLE_MESSAGE);
+            System.err.println("Please specify the correct parameters.");
+            System.err.println(EXAMPLE_MESSAGE);
             System.exit(1);
         }
 		
@@ -136,6 +165,8 @@ public class CreateFlowsheetConceptMapping {
 		if (mappingArgs == null) {
 			System.exit(1);
 		}
+		
+		System.exit(0);
 	}
 	
 }
