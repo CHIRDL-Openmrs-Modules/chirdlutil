d486efa4a87e3d94b0327c921e2d9c4e6ce9fc13
sjmckee <sjmckee@iupui.edu>
2022-09-16 10:38:13 -0400

    CHICA-1654 Velocity Vulnerability Fix (#177)
    Co-authored-by: sabhanda <sabhanda@140.182.66.200>

--- api/src/main/java/org/openmrs/module/chirdlutil/datatype/IntegerDatatype.java
+++ api/src/main/java/org/openmrs/module/chirdlutil/datatype/IntegerDatatype.java
@@ -1,6 +1,6 @@
 package org.openmrs.module.chirdlutil.datatype;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.openmrs.customdatatype.SerializingCustomDatatype;
 
 /**
